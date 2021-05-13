e777cc16480f62c57a7a2bcd2448cff679f5a27b
lxieqing@163.com <478040784@qq.com>
2021-05-13 10:26:24 +0800

    添加模板

--- generator.js
+++ generator.js
@@ -1,5 +1,19 @@
 // 负责注入或者修改项目中的文件
 
-module.exports = api => {
-  api.render('./template')
+module.exports = (api, options, rootOptions) => {
+  api.extendPackages({
+    scripts: {
+      test: "vue-cli-service command",
+    },
+  })
+
+  api.render("./template")
+
+  if (options.module === "module1") {
+    console.log(`Your chooices is ${options.module}`)
+  }
+
+  if (options.moduleName === "myModule") {
+    console.log(`Your input is ${options.moduleName}`)
+  }
 }
